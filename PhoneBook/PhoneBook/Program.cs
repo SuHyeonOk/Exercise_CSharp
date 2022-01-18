86c7d052e2f7d7cebf9428437fc63d4cfc2df0d7
SuHyeonOk <tntngus1223@naver.com>
2022-01-18 18:39:31 +0900

    전화번호부 오류수정 후 1차 완성

--- PhoneBook/PhoneBook/Program.cs
+++ PhoneBook/PhoneBook/Program.cs
@@ -5,25 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 /* 전화번호부 만들기
     삭제, 추가, 수정이 가능하도록*/
-//1
 namespace PhoneBook
 {
-    class Program
+    class PhoneBook
     {
-        static string[] name = new string[100];
-        static int[] phoneNumber = new int[100];
+        string[] name = new string[100];
+        int[] phoneNumber = new int[100];
 
-        static int _number = 101;
-
-        enum PhoneBook
-        {
-            None,
-            Find,
-            Addition,
-            Correction,
-            Delete
-        }
-        static void Sign()
+        public void EnterSign()
         {
             Console.WriteLine("\n" + "*---             전화번호부             ---*");
             Console.WriteLine("*--- 최대 100개의 전화번호가 저장됩니다 ---*");
@@ -32,14 +21,15 @@ namespace PhoneBook
             Console.WriteLine("*--- 3. 이름 + 전화번호 수정하기        ---*");
             Console.WriteLine("*--- 4. 이름 + 전화번호 삭제하기        ---*" + "\n");
         }
-        static void Find()
+        public void Find()
         {
             Console.WriteLine("*--- 1. 전화번호부 검색하기 ---*");
-            Console.WriteLine("1. 이름으로 찾고 싶어요.");
-            Console.WriteLine("2. 전화번호로 찾고 싶어요.");           
+            Console.WriteLine(" 1. 이름으로 찾고 싶어요.");
+            Console.WriteLine(" 2. 전화번호로 찾고 싶어요.");
             int find = int.Parse(Console.ReadLine());
 
-            if(find == 1) // 이름으로 찾기
+            int _number = -1;
+            if (find == 1) // 이름으로 찾기
             {
                 Console.WriteLine(" *이름으로 전화번호 찾기입니다.");
                 Console.WriteLine(" *찾으실 전화번호 이름을 입력해주세요.");
@@ -47,22 +37,20 @@ namespace PhoneBook
 
                 for (int i = 0; i < 100; i++)
                 {
-                    if(findName == name[i])
-                    {
+                    if (findName == name[i])
                         _number = i;
-                    }
                 }
-                if (findName == name[_number])
+                if (_number != -1)
                 {
-                    Console.WriteLine($"*{name[_number]}*님의 전화번호는");
-                    Console.WriteLine($"*{phoneNumber[_number]}* 입니다");
+                    Console.WriteLine($" *{name[_number]}*님의 전화번호는");
+                    Console.WriteLine($" *{phoneNumber[_number]}* 입니다");
                 }
-                else if (findName != null)
+                else
                 {
                     Console.WriteLine(" *입력하신 이름에 저장된 전화번호가 없습니다.");
                 }
             }
-            else if(find == 2) // 전화번호로 찾기
+            else if (find == 2) // 전화번호로 찾기
             {
                 Console.WriteLine(" *전화번호로 이름찾기 입니다.");
                 Console.WriteLine(" *찾으실 이름의 전화번호를 입력해주세요.");
@@ -71,16 +59,14 @@ namespace PhoneBook
                 for (int i = 0; i < 100; i++)
                 {
                     if (findPhoneNumber == phoneNumber[i])
-                    {
                         _number = i;
-                    }
                 }
-                if (findPhoneNumber == phoneNumber[_number])
+                if (_number != -1)
                 {
-                    Console.WriteLine($"{name[_number]}님의 전화번호는");
-                    Console.WriteLine($"{phoneNumber[_number]} 입니다");
+                    Console.WriteLine($" *{name[_number]}*님의 전화번호는");
+                    Console.WriteLine($" *{phoneNumber[_number]}* 입니다");
                 }
-                else if(findPhoneNumber != phoneNumber[_number])
+                else
                 {
                     Console.WriteLine(" *입력하신 전화번호에 저장된 전화번호가 없습니다.");
                 }
@@ -91,40 +77,45 @@ namespace PhoneBook
             }
         }
 
-        static void Addition()
+        public void Add()
         {
             Console.WriteLine("*--- 2. 이름 + 전화번호 추가하기 ---*");
             Console.WriteLine(" *추가할 이름을 입력해주세요.");
             string AddName = Console.ReadLine();
 
-            for(int i = 0; i < 100; i++)
+            int _number = -1;
+
+            for (int i = 0; i < 100; i++)
             {
-                if(name[i] == null)
+                if (name[i] == null)
                 {
-                    name[i] = AddName;
                     _number = i;
                 }
             }
+            name[_number] = AddName;
 
             Console.WriteLine(" *추가할 전화번호를 입력해주세요.");
             int AddPhoneNumber = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < 100; i++)
-            {
-                if (phoneNumber[i] == 0)
-                    phoneNumber[i] = AddPhoneNumber;
-            }
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (phoneNumber[i] == 0)
+            //        phoneNumber[i] = AddPhoneNumber;
+            //}
+            phoneNumber[_number] = AddPhoneNumber;
 
-            Console.WriteLine($"입력하신 *{name[_number]}*님의 전화번호");
-            Console.WriteLine($"*{phoneNumber[_number]}*이 추가되었습니다");
+            Console.WriteLine($" 입력하신 *{name[_number]}*님의 전화번호");
+            Console.WriteLine($" *{phoneNumber[_number]}*이 추가되었습니다");
         }
 
-        static void Correction()
+        public void Correct()
         {
             Console.WriteLine("*--- 3. 이름 + 전화번호 수정하기 ---*");
-            Console.WriteLine("1. 이름으로 수정 싶어요");
-            Console.WriteLine("2. 전화번호로 수정 싶어요");
+            Console.WriteLine(" 1. 이름으로 수정 싶어요");
+            Console.WriteLine(" 2. 전화번호로 수정 싶어요");
             int correction = int.Parse(Console.ReadLine());
+
+            int _number = -1;
 
             if (correction == 1) // 이름으로 수정
             {
@@ -132,16 +123,15 @@ namespace PhoneBook
                 Console.WriteLine(" *수정하실 전화번호 이름을 입력해주세요.");
                 string findName = Console.ReadLine();
 
+                // for문은 입력한 findName와 name배열에 있는 방의 이름이 같은 것을 찾아 그 방의 번호를 _number에 넣어주는 for문
                 for (int i = 0; i < 100; i++)
                 {
                     if (findName == name[i])
-                    {
                         _number = i;
-                    }
                 }
-                if (findName == name[_number])
+                if (_number != -1)
                 {
-                    Console.WriteLine($"{name[_number]}님의 이름 + 전화번호를 수정합니다");
+                    Console.WriteLine($" {name[_number]}님의 이름 + 전화번호를 수정합니다");
                     Console.WriteLine(" *수정할 이름을 입력해주세요.");
                     string correctionName = Console.ReadLine();
                     name[_number] = correctionName;
@@ -150,10 +140,8 @@ namespace PhoneBook
                     int correctionPhoneNumber = int.Parse(Console.ReadLine());
                     phoneNumber[_number] = correctionPhoneNumber;
 
-                    Console.WriteLine($"수정된 이름은 *{name[_number]}* 입니다");
-                    Console.WriteLine($"수정된 전화번호는 *{phoneNumber[_number]}* 입니다");
-
-                    _number = 101;
+                    Console.WriteLine($" 수정된 이름은 *{name[_number]}* 입니다");
+                    Console.WriteLine($" 수정된 전화번호는 *{phoneNumber[_number]}* 입니다");
                 }
                 else
                 {
@@ -169,13 +157,11 @@ namespace PhoneBook
                 for (int i = 0; i < 100; i++)
                 {
                     if (findPhoneNumber == phoneNumber[i])
-                    {
                         _number = i;
-                    }
                 }
-                if (findPhoneNumber == phoneNumber[_number])
+                if (_number != -1)
                 {
-                    Console.WriteLine($"{phoneNumber[_number]}님의 이름 + 전화번호를 수정합니다");
+                    Console.WriteLine($" {phoneNumber[_number]}의 이름 + 전화번호를 수정합니다");
                     Console.WriteLine(" *수정할 이름을 입력해주세요.");
                     string correctionName = Console.ReadLine();
                     name[_number] = correctionName;
@@ -184,10 +170,8 @@ namespace PhoneBook
                     int correctionPhoneNumber = int.Parse(Console.ReadLine());
                     phoneNumber[_number] = correctionPhoneNumber;
 
-                    Console.WriteLine($"수정된 이름은 *{name[_number]}* 입니다");
-                    Console.WriteLine($"수정된 전화번호는 *{phoneNumber[_number]}* 입니다");
-
-                    _number = 101;
+                    Console.WriteLine($" 수정된 이름은 *{name[_number]}* 입니다");
+                    Console.WriteLine($" 수정된 전화번호는 *{phoneNumber[_number]}* 입니다");
                 }
                 else
                 {
@@ -200,12 +184,14 @@ namespace PhoneBook
             }
         }
 
-        static void Delete()
+        public void Delete()
         {
             Console.WriteLine("*--- 4. 이름 + 전화번호 삭제하기 ---*");
-            Console.WriteLine("1. 이름으로 삭제하고 싶어요.");
-            Console.WriteLine("2. 전화번호로 삭제하고 싶어요.");
+            Console.WriteLine(" 1. 이름으로 삭제하고 싶어요.");
+            Console.WriteLine(" 2. 전화번호로 삭제하고 싶어요.");
             int find = int.Parse(Console.ReadLine());
+
+            int _number = -1;
 
             if (find == 1) // 이름으로 삭제
             {
@@ -220,22 +206,20 @@ namespace PhoneBook
                         _number = i;
                     }
                 }
-                if (findName == name[_number])
+                if (_number != -1)
                 {
-                    Console.WriteLine($"정말로 {findName}님의 이름 + 전화번호를 삭제하시겠습니까?");
-                    Console.WriteLine("1. 네");
-                    Console.WriteLine("2. 아니요");
+                    Console.WriteLine($" 정말로 {findName}님의 이름 + 전화번호를 삭제하시겠습니까?");
+                    Console.WriteLine(" 1. 네");
+                    Console.WriteLine(" 2. 아니요");
                     int really = int.Parse(Console.ReadLine());
 
                     if (really == 1)
                     {
                         name[_number] = null;
                         phoneNumber[_number] = 0;
-                        Console.WriteLine($"이름이 {findName}님이신 이름 + 전화번호를 삭제합니다.");
-
-                        _number = 101;
+                        Console.WriteLine($" 이름이 {findName}님이신 이름 + 전화번호를 삭제합니다.");
                     }
-                    else if(really == 2)
+                    else if (really == 2)
                     {
                         Console.WriteLine(" *삭제를 취소합니다");
                     }
@@ -262,20 +246,18 @@ namespace PhoneBook
                         _number = i;
                     }
                 }
-                if (findPhoneNumber == phoneNumber[_number])
+                if (_number != -1)
                 {
-                    Console.WriteLine($"정말로 {findPhoneNumber}님의 이름 + 전화번호를 삭제하시겠습니까?");
-                    Console.WriteLine("1. 네");
-                    Console.WriteLine("2. 아니요");
+                    Console.WriteLine($" 정말로 {findPhoneNumber}님의 이름 + 전화번호를 삭제하시겠습니까?");
+                    Console.WriteLine(" 1. 네");
+                    Console.WriteLine(" 2. 아니요");
                     int really = int.Parse(Console.ReadLine());
 
                     if (really == 1)
                     {
                         name[_number] = null;
                         phoneNumber[_number] = 0;
-                        Console.WriteLine($"전화번호가 {findPhoneNumber}님이신 이름 + 전화번호를 삭제합니다.");
-
-                        _number = 101;
+                        Console.WriteLine($" 전화번호가 {findPhoneNumber}님이신 이름 + 전화번호를 삭제합니다.");
                     }
                     else if (really == 2)
                     {
@@ -297,26 +279,40 @@ namespace PhoneBook
             }
         }
 
+    }
+    class Program
+    {
+        enum Menu
+        {
+            None,
+            Find,
+            Add,
+            Correction,
+            Delete
+        }
+
         static void Main(string[] args)
         {
             while (true)
             {
-                Sign();
+                PhoneBook phoneBook = new PhoneBook();
+                phoneBook.EnterSign();
+
                 int input = int.Parse(Console.ReadLine());
 
-                switch ((PhoneBook)input)
+                switch ((Menu)input)
                 {
-                    case PhoneBook.Find:
-                        Find();
+                    case Menu.Find:
+                        phoneBook.Find();
                         break;
-                    case PhoneBook.Addition:
-                        Addition();
+                    case Menu.Add:
+                        phoneBook.Add();
                         break;
-                    case PhoneBook.Correction:
-                        Correction();
+                    case Menu.Correction:
+                        phoneBook.Correct();
                         break;
-                    case PhoneBook.Delete:
-                        Delete();
+                    case Menu.Delete:
+                        phoneBook.Delete();
                         break;
                 }
             }
