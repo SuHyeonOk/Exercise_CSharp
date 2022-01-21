61a27b784b93fe6fcb1dac1d0027b554408808c9
SuHyeonOk <tntngus1223@naver.com>
2022-01-21 17:38:52 +0900

    Swap 함수

---
+++ Question_simple/Question_simple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_simple
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int c = a; // 1
+            a = b; // 
+            b = c;
+        }
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"{a}, {b}");
+        }
+    }
+}
